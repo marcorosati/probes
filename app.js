061e87580d26d60176eab88b9ecc8b9b984b2f8b
Marco Rosati <marcorosati_1@libero.it>
2019-11-05 16:25:43 -0500

    log

--- app.js
+++ app.js
@@ -7,27 +7,32 @@ var msg = process.env.APP_MSG;
 var response;
 
 app.get('/', function (req, res) {
+    console.log('request');
     res.status(200);
     res.send('Hello world');
 });
 
 app.get('/healthz', function (req, res) {
+    console.log('healthz');
     res.status(200);
     res.send();
 });
 
 app.get('/ready', function (req, res) {
+    console.log('ready');
     res.status(200);
     res.send();
 });
 
 
 app.get('/healthz-ko', function (req, res) {
+    console.log('healthz-ko');
     res.status(400);
     res.send();
 });
 
 app.get('/ready-ko', function (req, res) {
+    console.log('ready-ko');
     res.status(400);
     res.send();
 });
